b8cc807753812e6ab1c8df110c62c7c6b932f843
kubaleduchowski <leduchowskijakub@gmail.com>
2017-03-08 16:15:24 +0100

    create pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
   root 'home#index'
+  get 'home/about'
+  get 'home/contact'
+  get 'home/faq'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
